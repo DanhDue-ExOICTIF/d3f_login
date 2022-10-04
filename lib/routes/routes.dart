5235f1b0246bd33972eee3d525d0fc756c2aff94
DanhDue ExOICTIF <danhdue@gmail.com>
2022-10-04 16:22:49 +0700

    add the empty login page.

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,9 +1,11 @@
+import 'package:d3f_login/screens/login/login_page.dart';
 import 'package:d3f_login/screens/splash/splash_page.dart';
 import 'package:get/get.dart';
 import 'links.dart';
 
 class LoginAppRoutes {
-  static final pages = [
-    GetPage(name: LoginAppLinks.splash, page: () => SplashPage())
+  static final pages = <GetPage>[
+    GetPage(name: LoginAppLinks.splash, page: () => SplashPage()),
+    GetPage(name: LoginAppLinks.login, page: () => LoginPage())
   ];
 }
---
+++ lib/screens/login/login_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'login_controller.dart';
+
+class LoginPage extends StatelessWidget {
+  final logic = Get.find<LoginController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
