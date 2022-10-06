e5c598a22cd06c2b8ea4401a39284b9cb075c1d9
DanhDue ExOICTIF <danhdue@gmail.com>
2022-10-06 15:10:52 +0700

    refactor the login global binding.

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       translationsKeys: AppTranslation.translations,
       locale: const Locale('vi', 'VI'),
       fallbackLocale: const Locale('vi', 'VI'),
-      initialBinding: AppGlobalBinding(),
+      initialBinding: LoginGlobalBinding(),
       initialRoute: LoginAppLinks.splash,
       routingCallback: (routing) => LoginMiddleWare.observer,
       getPages: LoginAppRoutes.pages,
