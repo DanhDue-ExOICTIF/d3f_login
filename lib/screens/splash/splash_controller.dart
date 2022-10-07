39232d311d0a43d00324964e6a302cacfb9d9366
DanhDue ExOICTIF <danhdue@gmail.com>
2022-10-07 10:55:13 +0700

    add translations & refactor the splash screen.

--- lib/screens/splash/splash_controller.dart
+++ lib/screens/splash/splash_controller.dart
@@ -1,5 +1,13 @@
+import 'package:d3f_core/screens/base/base_controller.dart';
+import 'package:d3f_login/routes/links.dart';
 import 'package:get/get.dart';
 
-class SplashController extends GetxController {
+class SplashController extends BaseController {
+  final _animationComplete = false.obs;
 
+  void indicateAnimationComplete() {
+    _animationComplete.value = true;
+    printInfo(info: "Navigate to the dashboard screen.");
+    Get.offAllNamed(LoginAppLinks.login);
+  }
 }
---
+++ lib/translations.dart
@@ -0,0 +1,12 @@
+import 'package:d3f_login/d3f_login.dart';
+import 'package:d3f_shared/generated/locales.g.dart' as shared;
+
+abstract class LoginGlobalTranslation {
+  static Map<String, Map<String, String>> translationsKeys = {};
+
+  static Map<String, Map<String, String>> mergedTranslationKeys() {
+    translationsKeys.addAll(AppTranslation.translations);
+    translationsKeys.addAll(shared.AppTranslation.translations);
+    return translationsKeys;
+  }
+}
