2c046181926cf66fa67f401b26261446a591e061
DanhDue ExOICTIF <danhdue@gmail.com>
2022-10-07 09:57:51 +0700

    add the flutter_gen to the project.

--- lib/app.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class EnvironmentConfig {
-  static const APP_NAME = String.fromEnvironment('DART_DEFINES_APP_NAME',
-      defaultValue: "Login");
-  static const APP_SUFFIX = String.fromEnvironment('DART_DEFINES_APP_SUFFIX');
-  static const BASE_URL = String.fromEnvironment('DART_DEFINES_BASE_URL',
-      defaultValue: "https://danhdue.com/");
-}
