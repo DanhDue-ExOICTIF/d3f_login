f9a476314f2e2b6bbdfa343a1f3487dff215451f
DanhDue ExOICTIF <danhdue@gmail.com>
2023-01-21 12:11:26 +0700

    add Melos's scripts & login app signings.

--- lib/generated/assets.gen.dart
+++ lib/generated/assets.gen.dart
@@ -5,7 +5,7 @@
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
-// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal
+// ignore_for_file: directives_ordering,unnecessary_import
 
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -176,8 +176,6 @@ class SvgGenImage {
   }
 
   String get path => _assetName;
-
-  String get keyName => _assetName;
 }
 
 class LottieGenImage {
@@ -210,7 +208,6 @@ class LottieGenImage {
   }) {
     return Lottie.asset(
       _assetName,
-      controller: controller,
       animate: animate,
       frameRate: frameRate,
       repeat: repeat,
@@ -235,6 +232,4 @@ class LottieGenImage {
   }
 
   String get path => _assetName;
-
-  String get keyName => _assetName;
 }
--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -13,3 +13,9 @@ dart pub global activate flutter_gen
 
 # install get_cli
 dart pub global activate get_cli
+
+# install lcov to convert the lcov.info file to HTML pages.
+brew install lcov
+
+# install cloc to tracking file types.
+brew install cloc
