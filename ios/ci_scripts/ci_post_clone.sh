aa01d3bf3588ce508e7f656855287070555c9ff1
DanhDue ExOICTIF <danhdue@gmail.com>
2023-01-26 00:25:30 +0700

    modify the ci_post_clone.sh for the XCode Cloud.

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -3,15 +3,6 @@
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_WORKSPACE # change working directory to the root of your cloned repo.
 
-# Install CocoaPods using Homebrew.
-HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
-brew install cocoapods
-
-# Install CocoaPods dependencies.
-cd ios && pod install # run `pod install` in the `ios` directory.
-
-cd $CI_WORKSPACE # change working directory to the root of your cloned repo.
-
 # Install Flutter using git.
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
 export PATH="$PATH:$HOME/flutter/bin"
@@ -22,14 +13,19 @@ flutter precache --ios
 # Install Flutter dependencies.
 flutter pub get
 
-# Update generated files
-flutter pub run build_runner build
+# Install CocoaPods using Homebrew.
+HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
+brew install cocoapods
+
+# Install CocoaPods dependencies.
+cd ios && pod install # run `pod install` in the `ios` directory.
+
+cd $CI_WORKSPACE # change working directory to the root of your cloned repo.
 
 # Build ios app
-flutter build ipa --obfuscate --split-debug-info --split-per-abi \
+flutter build ios --obfuscate --split-debug-info --split-per-abi \
 --dart-define=DART_DEFINES_APP_NAME="D3FLogin" \
 --dart-define=DART_DEFINES_BASE_URL="https://danhdue.com/api/v1" \
---export-options-plist ./ios/fastlane/export_options/Release/ExportOptions.plist \
 --release
 
 exit 0
